6e3e36d2d948fe76b5640f5991e6e54bc3e78a23
geraldvindasramirez <geraldvindasramirez4@gmail.com>
2026-09-03 01:26:18 -0600

    Add files via upload

---
+++ ColaFIFO.hpp
@@ -0,0 +1,53 @@
+#ifndef COLAFIFO_HPP
+#define COLAFIFO_HPP
+
+#include "Nodo.h"
+
+template <typename Dato>
+class ColaFIFO {
+private:
+    Nodo<Dato>* apFrente;
+    Nodo<Dato>* apFondo;
+    int aTamano;
+
+public:
+    ColaFIFO() : apFrente(nullptr), apFondo(nullptr), aTamano(0) {}
+
+    ~ColaFIFO() {
+        while (!esVacia()) {
+            desencolar();
+        }
+    }
+
+    bool esVacia() const {
+        return apFrente == nullptr;
+    }
+
+    void encolar(Dato pValor) {
+        Nodo<Dato>* nuevo = new Nodo<Dato>(pValor);
+        if (esVacia()) {
+            apFrente = apFondo = nuevo;
+        } else {
+            apFondo->apSiguiente = nuevo;
+            apFondo = nuevo;
+        }
+        aTamano++;
+    }
+
+    Dato desencolar() {
+        if (esVacia()) return Dato();
+        Nodo<Dato>* temp = apFrente;
+        Dato valor = temp->aDato;
+        apFrente = apFrente->apSiguiente;
+        if (apFrente == nullptr) {
+            apFondo = nullptr;
+        }
+        delete temp;
+        aTamano--;
+        return valor;
+    }
+
+    int getTamano() const { return aTamano; }
+};
+
+#endif // COLAFIFO_HPP
---
+++ ListaEnlazada.hpp
@@ -0,0 +1,74 @@
+#ifndef LISTAENLAZADA_HPP
+#define LISTAENLAZADA_HPP
+
+#include "Nodo.h"
+
+template <typename Dato>
+class ListaEnlazada {
+private:
+    Nodo<Dato>* apCabeza;
+    int aTamano;
+
+public:
+    ListaEnlazada() : apCabeza(nullptr), aTamano(0) {}
+
+    ~ListaEnlazada() {
+        limpiar();
+    }
+
+    void limpiar() {
+        Nodo<Dato>* actual = apCabeza;
+        while (actual != nullptr) {
+            Nodo<Dato>* siguiente = actual->apSiguiente;
+            delete actual;
+            actual = siguiente;
+        }
+        apCabeza = nullptr;
+        aTamano = 0;
+    }
+
+    void agregarFinal(Dato pValor) {
+        Nodo<Dato>* nuevo = new Nodo<Dato>(pValor);
+        if (apCabeza == nullptr) {
+            apCabeza = nuevo;
+        } else {
+            Nodo<Dato>* temp = apCabeza;
+            while (temp->apSiguiente != nullptr) {
+                temp = temp->apSiguiente;
+            }
+            temp->apSiguiente = nuevo;
+        }
+        aTamano++;
+    }
+
+    int getTamano() const { return aTamano; }
+    Nodo<Dato>* getCabeza() const { return apCabeza; }
+
+    // Algoritmo de ordenamiento para las estadísticas
+    template <typename Comparador>
+    void ordenar(Comparador comp) {
+        if (apCabeza == nullptr || apCabeza->apSiguiente == nullptr) return;
+
+        Nodo<Dato>* ordenada = nullptr;
+        Nodo<Dato>* actual = apCabeza;
+
+        while (actual != nullptr) {
+            Nodo<Dato>* siguiente = actual->apSiguiente;
+            if (ordenada == nullptr || comp(actual->aDato, ordenada->aDato)) {
+                actual->apSiguiente = ordenada;
+                ordenada = actual;
+            } else {
+                Nodo<Dato>* temp = ordenada;
+                while (temp->apSiguiente != nullptr && !comp(actual->aDato, temp->apSiguiente->apSiguiente != nullptr ? temp->apSiguiente->aDato : actual->aDato)) {
+                    temp = temp->apSiguiente;
+                }
+                actual->apSiguiente = temp->apSiguiente;
+                temp->apSiguiente = actual;
+            }
+            actual = siguiente;
+        }
+        apCabeza = ordenada;
+    }
+};
+
+#endif // LISTAENLAZADA_HPP
